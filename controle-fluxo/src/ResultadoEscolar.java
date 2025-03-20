275b05987d3d0a1b1816f043143bccfbc9b6c5c3
Livia Pimentel <livia_macedo89@yahoo.com.br>
2025-03-20 16:00:30 -0300

    condição encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,11 +1,15 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 4;
 
-        if (nota >= 7) {
+        if (nota >= 7) 
             System.out.println("Aprovado");
-        } else {
+        
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova de Recuperação");
+
+        else 
             System.out.println("Reprovado");
-        }
+        
     }
 }
